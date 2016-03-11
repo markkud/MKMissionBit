8b1c8771fec4abd4cdfdd5d414a996b70d53867d
17jenim <17jenim@gatewayhigh.net>
2016-03-10 17:26:09 -0800

    The game character
    Ghost

--- GameDev/state0.js
+++ GameDev/state0.js
@@ -16,18 +16,25 @@ demo.state0.prototype = {
         var tree = game.add.sprite(0, 0, 'trees');
         adam = game.add.sprite(0,450, 'adam');
         adam.animations.add('walk',[0,1,2,3,4]);
+        adam.anchor.setTo(0.5, 0.5);
+        adam.scale.setTo(0.7, 0.7);
         game.physics.enable(adam);
         adam.body.collideWorldBounds = true;
+        game.camera.follow(adam);
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     },
 
 	update: function(){
       if (game.input.keyboard.isDown (Phaser.Keyboard.RIGHT)){
         adam.x = adam.x +speed;
         adam.animations.play('walk', 20, true); 
+          adam.scale.setTo(0.7, 0.7);
      }
         
     else  if (game.input.keyboard.isDown (Phaser.Keyboard.LEFT)){adam.x =adam.x - speed;
-                   adam.animations.play('walk', 20, true);                                        
+                   adam.animations.play('walk', 20, true); 
+                   adam.scale.setTo(-0.7, 0.7);                                              
+                                                                 
      }
          
     else{
@@ -35,8 +42,8 @@ demo.state0.prototype = {
 }    
      if (game.input.keyboard.isDown (Phaser.Keyboard.UP)){adam.y =adam.y - speed}
         
-        if (adam.y < 400){
-            adam.y =400;
+        if (adam.y < 100){
+            adam.y =100;
         }
         
      if (game.input.keyboard.isDown (Phaser.Keyboard.DOWN)){adam.y =adam.y + speed}
