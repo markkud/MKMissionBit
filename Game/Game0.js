794c081d1590ef9fd576b7341cfeb315b4d0038d
17jenim <17jenim@gatewayhigh.net>
2016-03-22 17:55:44 -0700

    Game

--- Game/Game0.js
+++ Game/Game0.js
@@ -1,11 +1,12 @@
 var demo = {};
-demo.state0 = function(){};
-//var adam;
-//var speed = 10;
-demo.state0.prototype = {
-	preload: function(){
-		game.load.image('trees', 'assets/background/treeBG.png');
-//		game.load.spritesheet('adam', 'assets/spritesheets/adamSpritesheet.png',240,370);
+demo.Game0 = function(){};
+var ghost;
+var speed = 10;
+demo.Game0.prototype = {
+	
+    preload: function(){
+		game.load.image('back', 'assets/Background.png');
+    game.load.spritesheet('ghost', 'assets/ghost.png',278,225);
         
 	},
 
@@ -13,11 +14,14 @@ demo.state0.prototype = {
 //        game.world.setBounds(0,0, 2813, 1000);
 //        game.physics.startSystem(Phaser.Physics.ARCADE);
         
-        var tree = game.add.sprite(0, 0, 'trees');
-//        adam = game.add.sprite(0,450, 'adam');
-//        adam.animations.add('walk',[0,1,2,3,4]);
+        var back = game.add.sprite(0, 0, 'back');
+        
+        back.height = game.height;
+        back.width = game.width;
+        ghost = game.add.sprite(0,0, 'ghost');
+//        ghost.animations.add('walk',[0,1,2]);
 //        adam.anchor.setTo(0.5, 0.5);
-//        adam.scale.setTo(0.7, 0.7);
+        ghost.scale.setTo(0.3, 0.3);
 //        game.physics.enable(adam);
 //        adam.body.collideWorldBounds = true;
 //        game.camera.follow(adam);
@@ -26,9 +30,9 @@ demo.state0.prototype = {
 
 	update: function(){
 //      if (game.input.keyboard.isDown (Phaser.Keyboard.RIGHT)){
-//        adam.x = adam.x +speed;
-//        adam.animations.play('walk', 20, true); 
-//          adam.scale.setTo(0.7, 0.7);
+//        ghost.x = ghost.x +speed;
+//        ghost.animations.play('walk', 20, true); 
+//          ghost.scale.setTo(0.3, 0.3);
 //     }
 //        
 //    else  if (game.input.keyboard.isDown (Phaser.Keyboard.LEFT)){adam.x =adam.x - speed;
@@ -47,6 +51,6 @@ demo.state0.prototype = {
 //        }
 //        
 //     if (game.input.keyboard.isDown (Phaser.Keyboard.DOWN)){adam.y =adam.y + speed}
-//	}
+	}
 
 };
